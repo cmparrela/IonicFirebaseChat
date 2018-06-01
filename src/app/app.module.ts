241ee129e5c3b532d7c802f689cb6ab4bc4180b9
Cristopher Parrela <cristopher.parrela@procedo.com.br>
2018-06-01 11:43:20 -0300

    Configurações do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { FirebaseAppConfig, AngularFireModule } from "angularfire2";
+
+const firebaseAppConfig: FirebaseAppConfig = {
+    apiKey: "AIzaSyCHQaH3ryXkPIMg-z0chT77hFKoO16j8GA",
+    authDomain: "udemy-curso-chat.firebaseapp.com",
+    databaseURL: "https://udemy-curso-chat.firebaseio.com",
+    projectId: "udemy-curso-chat",
+    storageBucket: "udemy-curso-chat.appspot.com",
+    messagingSenderId: "369076819731"
+};
+
 @NgModule({
     declarations: [
         MyApp,
@@ -14,7 +25,8 @@ import { HomePage } from '../pages/home/home';
     ],
     imports: [
         BrowserModule,
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(firebaseAppConfig)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
